ef09cc0fd4d279b48bda0e1d4fdd07f06ed490e4
clfcg <leontev_dma@mail.ru>
2023-03-20 15:26:46 +0300

    feat: added django project tfoms_mpi_site and app mpi

---
+++ tfomsno_mpi/tfomsno_mpi_site/tfomsno_mpi_site/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tfomsno_mpi_site project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfomsno_mpi_site.settings')
+
+application = get_wsgi_application()
