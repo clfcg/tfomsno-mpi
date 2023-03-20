1924e161eda3db1aeaf71ef4e650e2ef90ba1a2b
clfcg <leontev_dma@mail.ru>
2023-03-20 16:54:04 +0300

    feat: added first page - index.html

--- tfomsno_mpi/tfomsno_mpi_site/mpi/views.py
+++ tfomsno_mpi/tfomsno_mpi_site/mpi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    content = {
+        "title": "Мастер Пациент Индекс",
+        "content": "Все получилось",
+    }
+    return render(request, "mpi/index.html", content)
