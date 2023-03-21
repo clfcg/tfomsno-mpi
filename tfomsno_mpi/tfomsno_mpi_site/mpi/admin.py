6d550ae205a5b70d60721387092216de6d80d4b2
clfcg <leontev_dma@mail.ru>
2023-03-21 17:38:30 +0300

    feat: added nsi

--- tfomsno_mpi/tfomsno_mpi_site/mpi/admin.py
+++ tfomsno_mpi/tfomsno_mpi_site/mpi/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MpiServices, MpiMethods
+from .models import *
 
 
 class MpiServicesAdmin(admin.ModelAdmin):
@@ -15,5 +15,31 @@ class MpiMethodsAdmin(admin.ModelAdmin):
     list_editable = ("is_active",)
 
 
+class NsiSexAdmin(admin.ModelAdmin):
+    list_display = ("id", "caption")
+
+
+class NsiMpiPolisTypeAdmin(admin.ModelAdmin):
+    list_display = ("id","caption", "code", "is_active")
+    list_display_links = ("id", "caption")
+    list_editable = ("is_active",)
+
+
+class NsiDudlTypeAdmin(admin.ModelAdmin):
+    list_display = ("id","caption", "code", "m_ser", "m_num", "is_active")
+    list_display_links = ("id", "caption")
+    list_editable = ("is_active",)
+
+
+class MpiShowAdmin(admin.ModelAdmin):
+    list_display = ("id","caption", "desc", "is_active")
+    list_display_links = ("id", "caption")
+    list_editable = ("is_active",)
+
+
 admin.site.register(MpiServices, MpiServicesAdmin)
 admin.site.register(MpiMethods, MpiMethodsAdmin)
+admin.site.register(MpiShow, MpiShowAdmin)
+admin.site.register(NsiSex, NsiSexAdmin)
+admin.site.register(NsiMpiPolisType, NsiMpiPolisTypeAdmin)
+admin.site.register(NsiDudlType, NsiDudlTypeAdmin)
--- tfomsno_mpi/tfomsno_mpi_site/mpi/models.py
+++ tfomsno_mpi/tfomsno_mpi_site/mpi/models.py
@@ -2,33 +2,21 @@ from django.db import models
 
 
 class MpiServices(models.Model):
-    service_name = models.CharField(
-        max_length=150, 
-        verbose_name="Наименование"
-    )
-    service_description = models.CharField(
-        max_length=1000,
-        verbose_name="Описание"
-    )
-    service_url = models.CharField(
-        max_length=500,
-        verbose_name="Адрес WSDL"
-    )
-    is_active = models.BooleanField(
-        default=False,
-        verbose_name="Активен"
-    )
+    service_name = models.CharField(max_length=150, verbose_name="Наименование")
+    service_description = models.CharField(max_length=1000, verbose_name="Описание")
+    service_url = models.CharField(max_length=500, verbose_name="Адрес WSDL")
+    is_active = models.BooleanField(default=False, verbose_name="Активен")
 
     class Meta:
-        verbose_name = "Сервис МПИ"
-        verbose_name_plural = "Сервисы МПИ"
+        verbose_name = "МПИ Сервис"
+        verbose_name_plural = "МПИ Сервисы"
 
     def __str__(self):
         return self.service_name
 
 
 class MpiMethods(models.Model):
-    meth_name = models.CharField(max_length=100, verbose_name="Метод МПИ")
+    meth_name = models.CharField(max_length=100, verbose_name="Наименование")
     meth_desc = models.CharField(max_length=1000, verbose_name="Описание")
     mpi_service = models.ForeignKey(
         "MpiServices",
@@ -38,5 +26,60 @@ class MpiMethods(models.Model):
     is_active = models.BooleanField(default=False, verbose_name="Активен")
 
     class Meta:
-        verbose_name = "Метод МПИ"
-        verbose_name_plural = "Методы МПИ"
+        verbose_name = "МПИ Метод"
+        verbose_name_plural = "МПИ Методы"
+
+    def __str__(self):
+        return self.meth_name
+
+
+class NsiSex(models.Model):
+    caption = models.CharField(max_length=15, verbose_name="Наименование")
+
+    class Meta:
+        verbose_name = "Пол"
+        verbose_name_plural = "НСИ Пол"
+
+    def __str__(self):
+        return self.caption
+
+
+class NsiMpiPolisType(models.Model):
+    caption = models.CharField(max_length=100, verbose_name="Наименование")
+    code = models.CharField(max_length=5, verbose_name="Код")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    class Meta:
+        verbose_name = "Тип полиса"
+        verbose_name_plural = "МПИ Типы полисов"
+
+    def __str__(self):
+        return self.caption
+
+
+class NsiDudlType(models.Model):
+    caption = models.CharField(max_length=150, verbose_name="Наименование")
+    code = models.CharField(max_length=2, verbose_name="Код")
+    m_ser = models.CharField(max_length=10, verbose_name="Маска серии")
+    m_num = models.CharField(max_length=15, verbose_name="Маска номера")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    class Meta:
+        verbose_name = "Тип документа"
+        verbose_name_plural = "НСИ Типы документов"
+
+    def __str__(self):
+        return self.caption
+
+
+class MpiShow(models.Model):
+    caption = models.CharField(max_length=15, verbose_name="Наименование")
+    desc = models.CharField(max_length=100, verbose_name="Описание сущности")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    class Meta:
+        verbose_name = "Элемент сущности"
+        verbose_name_plural = "МПИ Список сущностей"
+
+    def __str__(self):
+        return self.caption
